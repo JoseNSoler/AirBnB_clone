0c9f6bac00230e065df9ee9a875269a28ae287d7
Diego Lopez <dfelo93@gmail.com>
2021-02-11 18:00:27 -0500

    Added do_create definition.

--- console.py
+++ console.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 import cmd
+from models.base_model import BaseModel
+import models
+
 
 class HBNBCommand(cmd.Cmd):
     """ command console for managing and interacting with the AirBnB clone """
@@ -10,7 +13,23 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         pass
 
-    # Implementations.--------------------------------------------------------|
+
+    # Implementations 0.1-----------------------------------------------------|
+    def do_create(self, line):
+        """Create a new BaseModel instance.\n"""
+        if line:
+            if line == 'BaseModel':
+                x = BaseModel()
+                models.storage.new(x)
+                models.storage.save()
+                print(x.id)
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+
+    # Implementations 0.0.1---------------------------------------------------|
     def do_quit(self, line):
         """Exits the program.\n"""
         return True
