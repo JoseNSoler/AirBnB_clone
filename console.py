4b64b50d7dc40b0aea34671661cc02f18de7bb3a
Diego Lopez <dfelo93@gmail.com>
2021-02-12 01:40:08 -0500

    Added intro message.

--- console.py
+++ console.py
@@ -4,16 +4,18 @@ from models.base_model import BaseModel
 from models.__init__ import storage
 import models
 
+
 class HBNBCommand(cmd.Cmd):
     """ command console for managing and interacting with the AirBnB clone """
 
-    prompt="(hbnb) "  #<--- Defines the look of the prompt
+    prompt = "(hbnb) "  # <--- Defines the look of the prompt
 
     # Definition override for handling ENTER with an empty line.--------------|
     def emptyline(self):
         pass
 
     # Implementations 0.1-----------------------------------------------------|
+    #                                                                         |
     # do_create - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
     def do_create(self, line):
         """Create a new BaseModel instance.\n"""
@@ -43,7 +45,7 @@ class HBNBCommand(cmd.Cmd):
                         if string == obj_id:
                             print(obj)
                             switch = True
-                    if switch == False:
+                    if switch is False:
                         print("** no instance found **")
                 else:
                     print("** instance id missing **")
@@ -59,7 +61,7 @@ class HBNBCommand(cmd.Cmd):
             args = line.split(" ")
             if args[0] == 'BaseModel':
                 if args[1]:
-                    if models.storage.delete(args[1]) == False:
+                    if models.storage.delete(args[1]) is False:
                         print("** no instance found **")
                 else:
                     print("** instance id missing **")
@@ -68,14 +70,49 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class name missing **")
 
+    # do_destroy - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -|
+    def do_all(self, line):
+        if line == "BaseModel" or line == "":
+            dicto = models.storage.all()
+            list_of_strings = []
+            for obj_id, obj in dicto.items():
+                list_of_strings.append(obj.__str__())
+            print(list_of_strings)
+        else:
+            print("** class doesn't exist **")
+
+    # do_update - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+    def do_update(self, line):
+        pass
+
     # Implementations 0.0.1---------------------------------------------------|
+    #                                                                         |
+    # do_quit - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
     def do_quit(self, line):
         """Exits the program.\n"""
         return True
 
+    # do_EOF - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -|
     def do_EOF(self, line):
         """Exits the program.\n"""
         return True
 
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+
+    green = "\033[92m"
+    reset = "\033[0m"
+
+    hlp = green + "help" + reset
+    qut = green + "quit" + reset
+    title_string = green + "Welcome to the hbnb C.L.I" + reset
+
+    intro_string = "._______________________________.\n" \
+                   "|                               |\n" \
+                   "|   " + title_string + "   |\n" \
+                   "|                               |\n" \
+                   "|     for help, type '" + hlp + "'     |\n" \
+                   "|      to quit, type '" + qut + "'     |\n" \
+                   "|                               |\n" \
+                   "'- - - - - - - - - - - - - - - -'"
+
+    HBNBCommand().cmdloop(intro_string)
