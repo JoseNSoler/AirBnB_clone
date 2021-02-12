8cb5ba78873cecec03daceb265752f53a8a9562e
Diego Lopez <dfelo93@gmail.com>
2021-02-11 19:59:39 -0500

    Added do_destroy method.

--- console.py
+++ console.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import cmd
 from models.base_model import BaseModel
+from models.__init__ import storage
 import models
-
 
 class HBNBCommand(cmd.Cmd):
     """ command console for managing and interacting with the AirBnB clone """
@@ -13,8 +13,8 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         pass
 
-
     # Implementations 0.1-----------------------------------------------------|
+    # do_create - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
     def do_create(self, line):
         """Create a new BaseModel instance.\n"""
         if line:
@@ -28,6 +28,45 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class name missing **")
 
+    # do_create - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+    def do_show(self, line):
+        """ Shows the BaseModel instance given its id. """
+        if line:
+            args = line.split(" ")
+            if args[0] == 'BaseModel':
+                if args[1]:
+                    string = "BaseModel." + "{}".format(args[1])
+                    models.storage.reload()
+                    dicto = models.storage.all()
+                    switch = False
+                    for obj_id, obj in dicto.items():
+                        if string == obj_id:
+                            print(obj)
+                            switch = True
+                    if switch == False:
+                        print("** no instance found **")
+                else:
+                    print("** instance id missing **")
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+    # do_destroy - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -|
+    def do_destroy(self, line):
+        """ Deletes an instance of BaseModel given its id. """
+        if line:
+            args = line.split(" ")
+            if args[0] == 'BaseModel':
+                if args[1]:
+                    if models.storage.delete(args[1]) == False:
+                        print("** no instance found **")
+                else:
+                    print("** instance id missing **")
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
 
     # Implementations 0.0.1---------------------------------------------------|
     def do_quit(self, line):
