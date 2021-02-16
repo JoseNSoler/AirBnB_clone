ac016f764117b79e38c42bc76bd187fbfe88665c
JoseNSoler <josenico1212@gmail.com>
2021-02-16 07:30:00 +0000

    FIX comments

--- models/city.py
+++ models/city.py
@@ -4,6 +4,12 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """ City class with name and state_id. """
+    """
+    City class with name and state_id.
+
+    Attributes:
+        state_id (str) = state_id to which it belongs
+        name (str) = name of City
+    """
     state_id = ""
     name = ""
