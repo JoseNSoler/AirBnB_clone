7b9b3aa7dec92610abdd0b8cdb27cae1071b9f42
JoseNSoler <josenico1212@gmail.com>
2021-02-16 06:21:53 +0000

    FIX mispell

--- console.py
+++ console.py
@@ -591,4 +591,4 @@ if __name__ == '__main__':
               "|_____________________________________________________|\n"
 
         # Start running the cmd loop
-        HBNBCommand().cmdloop(air)
+        HBNBCommand().cmdloop()
