0d7f71b4df5f276879d2b48a295a3e308f25bac2
JoseNSoler <josenico1212@gmail.com>
2021-02-16 22:47:12 +0000

    UPDATE more tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,6 +2,7 @@
 """ Module for storing the tests for BaseModel instances. """
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.user import User
 import models
 import uuid
 import unittest
@@ -71,17 +72,43 @@ class TestFileStorage(unittest.TestCase):
     def test_new(self):
         '''test new instance in objects'''
 
-        base = BaseModel()
+        base_x = BaseModel()
         with self.assertRaises(TypeError):
-            models.storage.new(base, 1)
+            models.storage.new(base_x, 1)
+        tempo_dict = models.storage.all()
 
-    def test_JSON_file(self):
-        ''' Tests for the file.json. '''
-        base = BaseModel()
-        base.save()
+        base_x_id = "BaseModel" + "." + base_x.id
+
+
+        user_x = User()
+        user_x.email = "pruebas@prueba.com"
+        user_x.first_name = "Don"
+        user_x.last_name = "Pruebas"
+        user_x.password = "tambienprueba"
+
+        user_x_id = "User." + user_x.id
+
+        for key, value in tempo_dict.items():
+            if key == user_x_id:
+                self.assertTrue(hasattr(user_x, "email"))
+                self.assertTrue(hasattr(user_x, "first_name"))
+                self.assertTrue(hasattr(user_x, "last_name"))
+                self.assertTrue(hasattr(user_x, "password"))
+                self.assertTrue(hasattr(user_x, "id"))
+
+
+        self.assertIn(base_x_id, tempo_dict.keys())
+
+        self.assertIn(user_x_id, tempo_dict.keys())
+
+    
+        models.storage.save()
+        #Test file created after new
         pwd = os.getcwd()
         path = pwd + "/file.json"
         self.assertTrue(os.path.exists(path))
+
+        
 
 if __name__ == '__main__':
     unittest.main()
